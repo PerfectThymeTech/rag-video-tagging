b7b82f95101d8c0d9f5532586f9800d3d49364fd
Marvin Buss <marvin.buss@gmail.com>
2024-04-02 21:36:09 +0200

    Add start workflow model

--- code/durablefunction/models/startworkflow.py
+++ code/durablefunction/models/startworkflow.py
@@ -1,13 +1,17 @@
 from typing import Any, Dict
+from enum import Enum
 
 from pydantic import BaseModel, Field
 from pydantic.dataclasses import dataclass
 from shared.config import settings
 
+class OrchestratorWorkflowEnum(str, Enum):
+    VIDEOEXTRACTION = "video_extraction_orchestrator"
+
 
 @dataclass
 class StartWorkflowRequest(BaseModel):
-    orchestrator_workflow_name: str = Field(
+    orchestrator_workflow_name: OrchestratorWorkflowEnum = Field(
         default=settings.FUNCTION_DEFAULT_ORCHESTRATOR_NAME,
         alias="orchestrator_workflow_name",
     )
