15077c3048967fab003dafa1a8012b2a16203c97
Marvin Buss <marvin.buss@gmail.com>
2024-04-02 21:41:58 +0200

    Update main entry point

--- code/durablefunction/function_app.py
+++ code/durablefunction/function_app.py
@@ -1,8 +1,9 @@
+import json
+
 import azure.durable_functions as df
 import azure.functions as func
-from models.start_workflow import StartWorkflowRequest
+from models.startworkflow import StartWorkflowRequest
 from pydantic import TypeAdapter
-from shared.config import settings
 from videoextraction.orchestration import bp as bp_videoextraction
 
 app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -15,11 +16,14 @@ app.register_functions(bp_videoextraction)
 @app.durable_client_input(client_name="client")
 async def http_start(req: func.HttpRequest, client: df.DurableOrchestrationClient):
     # Parse body
-    body: StartWorkflowRequest = TypeAdapter(StartWorkflowRequest).validate_json(
-        req.get_json()
+    payload: str = json.loads(req.get_body().decode())
+    payload_obj: StartWorkflowRequest = TypeAdapter(StartWorkflowRequest).validate_json(
+        req.get_body()
     )
 
     # Start orchestrator
-    instance_id = await client.start_new(body.orchestrator_workflow_name)
+    instance_id = await client.start_new(
+        payload_obj.orchestrator_workflow_name, client_input=payload
+    )
     response = client.create_check_status_response(req, instance_id)
     return response
