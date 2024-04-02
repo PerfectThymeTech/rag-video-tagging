f37a82e991a36de75e113b027903d271aa932f4e
Marvin Buss <marvin.buss@gmail.com>
2024-04-02 21:35:41 +0200

    Update util functions

--- code/durablefunction/shared/utils.py
+++ code/durablefunction/shared/utils.py
@@ -1,3 +1,8 @@
+import logging
+import os
+import shutil
+
+
 def download_blob():
     pass
 
@@ -6,5 +11,12 @@ def upload_blob():
     pass
 
 
-def delete_directory():
-    pass
+def delete_directory(path: str):
+    logging.info(f"Start removing directory '{path}' recursively.")
+    if os.path.exists(path=path):
+        shutil.rmtree(path=path)
+    else:
+        logging.error(f"Provided directory path '{path}' does not exist.")
+        raise ValueError(f"Provided directory path '{path}' does not exist.")
+
+    logging.info(f"Finished removing directory '{path}' recursively.")
