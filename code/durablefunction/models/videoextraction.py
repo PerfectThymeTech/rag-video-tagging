ef9bea05dd3b2de52a1145cfad2a821001397889
Marvin Buss <marvin.buss@gmail.com>
2024-04-02 22:05:55 +0200

    Add data models

--- code/durablefunction/models/videoextraction.py
+++ code/durablefunction/models/videoextraction.py
@@ -1,5 +1,5 @@
-from typing import List
 import logging
+from typing import List
 
 from pydantic import BaseModel, HttpUrl, field_validator
 from pydantic.dataclasses import dataclass
@@ -21,25 +21,60 @@ class VideoExtractionOrchestratorRequest(BaseModel):
     def check_content_url(cls, u: HttpUrl):
         if u.scheme != "https":
             logging.error(f"Scheme provided in content url: {u.port}")
-            raise ValueError(f"Please provide a valid blob storage url with an 'https' scheme. Provided value '{u.scheme}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url with an 'https' scheme. Provided value '{u.scheme}'."
+            )
         elif not u.host.endswith(".blob.core.windows.net"):
             logging.error(f"Host provided in content url: {u.port}")
-            raise ValueError(f"Please provide a valid blob storage url with a '.blob.core.windows.net' host url. Provided value '{u.host}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url with a '.blob.core.windows.net' host url. Provided value '{u.host}'."
+            )
         elif not u.path or len(u.path.split("/")) <= 1:
             logging.error(f"Path provided in content url: {u.port}")
-            raise ValueError(f"Please provide a valid blob storage url with a container and file name. Provided value '{u.path}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url with a container and file name. Provided value '{u.path}'."
+            )
         elif u.port and u.port != 443:
             logging.error(f"Port provided in content url: {u.port}")
-            raise ValueError(f"Please provide a valid blob storage url without any port parameters. Provided value '{u.port}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url without any port parameters. Provided value '{u.port}'."
+            )
         elif u.query:
             logging.error(f"Query provided in content url: {u.query}")
-            raise ValueError(f"Please provide a valid blob storage url without any query parameters. Provided value '{u.query}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url without any query parameters. Provided value '{u.query}'."
+            )
         elif u.fragment:
             logging.error(f"Fragment provided in content url: {u.fragment}")
-            raise ValueError(f"Please provide a valid blob storage url without any url fragments. Provided value '{u.fragment}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url without any url fragments. Provided value '{u.fragment}'."
+            )
         elif u.username:
             logging.error(f"Username provided in content url: {u.username}")
-            raise ValueError(f"Please provide a valid blob storage url without a username. Provided value '{u.username}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url without a username. Provided value '{u.username}'."
+            )
         elif u.password:
             logging.error(f"Password provided in content url: {u.password}")
-            raise ValueError(f"Please provide a valid blob storage url without a password. Provided value '{u.password}'.")
+            raise ValueError(
+                f"Please provide a valid blob storage url without a password. Provided value '{u.password}'."
+            )
+
+
+class DownloadVideoRequest(BaseModel):
+    storage_account_name: str
+    storage_container_name: str
+    storage_blob_name: str
+    instance_id: str
+
+
+class UploadVideoRequest(BaseModel):
+    video_file_path: str
+    storage_account_name: str
+    storage_container_name: str
+    instance_id: str
+
+
+class DeleteVideoRequest(BaseModel):
+    video_file_path: str
+    instance_id: str
