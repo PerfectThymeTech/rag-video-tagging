63a60481c4c3e2c2afcb8a00d3de9e1e3fc59253
Marvin Buss <marvin.buss@gmail.com>
2024-04-03 13:35:59 +0200

    Update config

--- code/durablefunction/shared/config.py
+++ code/durablefunction/shared/config.py
@@ -9,11 +9,12 @@ class Settings(BaseSettings):
     APP_VERSION: str = "v0.0.1"
     LOGGING_LEVEL: int = logging.INFO
     HOME_DIRECTORY: str = Field(default="", alias="HOME")
-    STORAGE_ACCOUNT_NAME: str = Field(
-        default="rgdurablefunctiona8c3", alias="STORAGE_ACCOUNT_NAME"
+    STORAGE_DOMAIN_NAME: str = Field(
+        default="rgdurablefunctiona8c3.blob.core.windows.net",
+        alias="STORAGE_DOMAIN_NAME",
     )
     STORAGE_ACCOUNT_CONTAINER: str = Field(
-        default="video", alias="STORAGE_ACCOUNT_CONTAINER"
+        default="video", alias="STORAGE_ACCOUNT_CONTAINER", min_length=3, max_length=63
     )
     FUNCTION_DEFAULT_ORCHESTRATOR_NAME: str = "video_extraction_orchestrator"
 
