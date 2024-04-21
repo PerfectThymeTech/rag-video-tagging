82f712868c75a6fa3411ee02dd5e719d9947da3d
Marvin Buss <marvin.buss@gmail.com>
2024-04-21 22:18:16 +0200

    Update llm messages and class

--- code/durablefunction/newstagextraction/llm.py
+++ code/durablefunction/newstagextraction/llm.py
@@ -1,105 +1,102 @@
 import logging
-import os
 
 from azure.identity import DefaultAzureCredential
 from langchain_core.messages import SystemMessage
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import AzureChatOpenAI
-from shared.config import settings
+from models.newstagextraction import InvokeLlmResponse
 
 
 class LlmMessages:
     SYSTEM_MESSAGE: str = """
-    You are a world class assistent for summarizing news content.
+    You are a world class assistant for summarizing news content.
     You extract subsections from the provided news content and generate a title for each subsection.
-    For each subsection you provide a score indicating how good the defined tags match the content of the subsection. 0 indicates that the tags don't match the content, and 1 means that the tags are a perfect match.
+    For each subsection you provide a score between 0 and 10 indicating how good the defined tags match the content of the subsection. 0 indicates that the tags don't match the content, and 10 means that the tags are a perfect match.
     You must include the start and end of the original text for each subsection in the response. The text snippet describing the beginning and end should include 5 words.
     You add tags to each subsection. Samples for tags are: sports, weather, international news, national news, politics, technology, celebrity, other. You add additional tags based on the content of each subsection.
 
     You always respond with the following JSON structure:
-    [
-        {
-            "title": "<title of the first subsection>",
-            "tags": "<tags of the first subsection>",
-            "score": "<score of the first subsection>",
-            "start": "<start of the text of the first subsection from the original text>",
-            "end": "<end of the text of the first subsection from the original text>"
-        },
-        {
-            "title": "<title of the second subsection>",
-            "tags": "<tags of the second subsection>",
-            "score": "<score of the second subsection>",
-            "start": "<start of the text of the second subsection from the original text>",
-            "end": "<end of the text of the second subsection from the original text>"
-        },
-        {
-            "title": "<title of the third subsection>",
-            "tags": "<tags of the third subsection>",
-            "score": "<score of the third subsection>",
-            "start": "<start of the text of the third subsection from the original text>",
-            "end": "<end of the text of the third subsection from the original text>"
-        }
-    ]
+    {format_instructions}
     """
     USER_MESSAGE: str = """
     News Content: "{news_content}"
     ---
-    Provide a summary for the provided news text. The text is about {news_show_details}
+    Provide a summary for the provided news text. The text is from the following tv show: {news_show_details}
     """
 
 
 class LlmInteractor:
     def __init__(
         self,
+        azure_open_ai_base_url,
+        azure_open_ai_api_version,
+        azure_open_ai_deployment_name,
     ) -> None:
         # Create llm chain
-        self.llm_chain = self._create_llm_chain()
+        self.__create_llm_chain(
+            azure_open_ai_base_url=azure_open_ai_base_url,
+            azure_open_ai_api_version=azure_open_ai_api_version,
+            azure_open_ai_deployment_name=azure_open_ai_deployment_name,
+        )
 
-    def _create_llm_chain(
+    def __create_llm_chain(
         self,
-    ):
+        azure_open_ai_base_url,
+        azure_open_ai_api_version,
+        azure_open_ai_deployment_name,
+    ) -> None:
         # Create chat prompt template
         logging.debug("Creating chat prompt template")
         prompt = ChatPromptTemplate.from_messages(
             [
                 SystemMessage(content=LlmMessages.SYSTEM_MESSAGE, type="system"),
                 ("user", LlmMessages.USER_MESSAGE),
-            ]
+            ],
         )
-        prompt.input_variables = ["news_content", "news_show_details"]
+        prompt.input_variables = [
+            "format_instructions",
+            "news_content",
+            "news_show_details",
+        ]
 
         # Create the llm
         logging.debug("Creating the llm")
-        os.environ["OPENAI_API_TYPE"] = "azure_ad"
+
+        def entra_id_token_provider():
+            credential = DefaultAzureCredential()
+            token = credential.get_token(
+                "https://cognitiveservices.azure.com/.default"
+            ).token
+            return token
+
         llm = AzureChatOpenAI(
-            azure_endpoint=settings.AZURE_OPEN_AI_BASE_URL,
-            api_version=settings.AZURE_OPEN_AI_API_VERSION,
-            deployment_name=settings.AZURE_OPEN_AI_DEPLOYMENT_NAME,
+            azure_endpoint=azure_open_ai_base_url,
+            api_version=azure_open_ai_api_version,
+            deployment_name=azure_open_ai_deployment_name,
+            azure_ad_token_provider=entra_id_token_provider,
         )
 
         # Create the output parser
         logging.debug("Creating the output parser")
-        output_parser = JsonOutputParser()
+        output_parser = JsonOutputParser(pydantic_object=InvokeLlmResponse)
+
+        # Insert partial into prompt
+        prompt_partial = prompt.partial(
+            format_instructions=output_parser.get_format_instructions()
+        )
+        logging.info(f"Prompt: {prompt.json()}")
 
         # Create chain
         logging.debug("Creating the llm chain")
-        chain = prompt | llm | output_parser
-        return chain
+        self.__llm_chain = prompt_partial | llm | output_parser
 
     def invoke_llm_chain(
         self,
         news_content: str,
         news_show_details: str,
-    ):
-        # Generate token for azure open ai and set as environment variable
-        credential = DefaultAzureCredential()
-        token = credential.get_token(
-            "https://cognitiveservices.azure.com/.default"
-        ).token
-        os.environ["OPENAI_API_KEY"] = token
-
-        result = self.llm_chain.invoke(
+    ) -> InvokeLlmResponse:
+        result: InvokeLlmResponse = self.__llm_chain.invoke(
             {"news_content": news_content, "news_show_details": news_show_details}
         )
         return result
