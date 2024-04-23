19fcfe70d165b06166d2ccd18639ef5842ffb105
Marvin Buss <marvin.buss@gmail.com>
2024-04-23 23:54:02 +0200

    Add orchestrator activities for timestamp processing

--- code/durablefunction/newstagextraction/orchestration.py
+++ code/durablefunction/newstagextraction/orchestration.py
@@ -1,16 +1,21 @@
 import json
 import logging
+import os
 from typing import Any, Dict
 
 import azure.durable_functions as df
 from models.error import ErrorModel
 from models.newstagextraction import (
+    ComputeTimestampsItem,
+    ComputeTimestampsRequest,
+    ComputeTimestampsResponse,
     InvokeLlmRequest,
     InvokeLlmResponse,
     LoadVideoindexerContentRequest,
     LoadVideoindexerContentResponse,
     NewsTagExtractionOrchestratorRequest,
 )
+from newstagextraction import timestamps
 from newstagextraction.llm import LlmInteractor
 from pydantic import ValidationError
 from shared import utils
@@ -86,6 +91,7 @@ def newstag_extraction_orchestrator(context: df.DurableOrchestrationContext):
     input_invoke_llm: InvokeLlmRequest = InvokeLlmRequest(
         content_text=result_load_videoindexer_content.transcript_text,
         content_details="This is a tv news show.",
+        instance_id=context.instance_id,
     )
     result_invoke_llm: InvokeLlmResponse = yield context.call_activity_with_retry(
         name="invoke_llm",
@@ -93,14 +99,33 @@ def newstag_extraction_orchestrator(context: df.DurableOrchestrationContext):
         input_=input_invoke_llm,
     )
 
-    # TODO: Generate timestamps
+    # Compute timestamps
+    logging.info("Compute timestamps")
+    utils.set_custom_status(
+        context=context,
+        completion_percentage=15.0,
+        status="Compute timestamps",
+    )
+    input_compute_timestamps: ComputeTimestampsRequest = ComputeTimestampsRequest(
+        result_video_indexer=result_load_videoindexer_content.transcript,
+        result_llm=result_invoke_llm.root,
+        instance_id=context.instance_id,
+    )
+    result_compute_timestamps: ComputeTimestampsResponse = (
+        yield context.call_activity_with_retry(
+            name="compute_timestamps",
+            retry_options=retry_options,
+            input_=input_compute_timestamps,
+        )
+    )
+
     # TODO: Get scene images from Video Indexer
 
     # Create output
     utils.set_custom_status(
         context=context, completion_percentage=100.0, status="Completed"
     )
-    result = {"error_code": 0, "text": result_invoke_llm.model_dump()}
+    result = {"error_code": 0, "text": result_compute_timestamps.model_dump()}
     return result
 
 
@@ -155,6 +180,15 @@ async def load_videoindexer_content(
     response: LoadVideoindexerContentResponse = LoadVideoindexerContentResponse(
         transcript_text=transcript_text, transcript=transcript_list
     )
+
+    # Upload result
+    await utils.upload_string(
+        data=response.model_dump_json(),
+        storage_domain_name=settings.STORAGE_DOMAIN_NAME,
+        storage_container_name=settings.STORAGE_CONTAINER_NAME,
+        storage_blob_name=os.path.join(inputData.instance_id, "transcript.json"),
+    )
+
     return response
 
 
@@ -171,9 +205,127 @@ async def invoke_llm(inputData: InvokeLlmRequest) -> InvokeLlmResponse:
         news_content=inputData.content_text,
         news_show_details=inputData.content_details,
     )
-    logging.info(f"Type: {type(llm_result)}")
     logging.info(f"LLM response: {json.dumps(llm_result)}")
 
     # Generate response
-    response: InvokeLlmResponse = InvokeLlmResponse(subsections=llm_result)
+    response: InvokeLlmResponse = InvokeLlmResponse(root=llm_result)
+
+    # Upload result
+    await utils.upload_string(
+        data=response.model_dump_json(),
+        storage_domain_name=settings.STORAGE_DOMAIN_NAME,
+        storage_container_name=settings.STORAGE_CONTAINER_NAME,
+        storage_blob_name=os.path.join(inputData.instance_id, "llm.json"),
+    )
+
+    return response
+
+
+@bp.activity_trigger(input_name="inputData")  # , activity="InvokeLlm")
+async def compute_timestamps(
+    inputData: ComputeTimestampsRequest,
+) -> ComputeTimestampsResponse:
+    # Configure index
+    result_llm_index = 0
+    result_llm_item_start = True
+
+    # Configure current item
+    result_llm_current_words = timestamps.get_cleansed_llm_response_item(
+        list=inputData.result_llm, index=result_llm_index, start=result_llm_item_start
+    )
+
+    # Generate response
+    response: ComputeTimestampsResponse = ComputeTimestampsResponse(root=[])
+    response_item = ComputeTimestampsItem(
+        title=inputData.result_llm[result_llm_index].title,
+        tags=inputData.result_llm[result_llm_index].tags,
+        score=inputData.result_llm[result_llm_index].score,
+        start_time="",
+        end_time="",
+    )
+
+    # Loop through transcript items
+    for i, item in enumerate(inputData.result_video_indexer):
+        # Split item into words
+        item_words = timestamps.cleanse_text(text=item.text)
+
+        # Loop through words
+        for j, item_word in enumerate(item_words):
+            # print(f"Response: {response.model_dump_json()}")
+            # print(f"Item: {response_item.model_dump_json()}")
+            # print(f"LLM Current Words: {result_llm_current_words}")
+            # print(f"item word: {item_word}")
+
+            if item_word == result_llm_current_words[0]:
+                # Create list of next words
+                remaining_words = item_words[j:]
+                num_words_missing = len(result_llm_current_words) - len(remaining_words)
+                start_time = item.instances[0].start
+                end_time = item.instances[0].end
+
+                if num_words_missing > 0 and (i + 1) < len(
+                    inputData.result_video_indexer
+                ):
+                    next_item = inputData.result_video_indexer[i + 1]
+                    next_item_words = timestamps.cleanse_text(text=next_item.text)
+
+                    # Update values for comparison
+                    remaining_words.extend(next_item_words[:num_words_missing])
+                    end_time = next_item.instances[0].end
+
+                # print(f"Remaining: {remaining_words}")
+                # Compute whether following items are identical
+                identical = [
+                    word_llm == remaining_words[k]
+                    for k, word_llm in enumerate(result_llm_current_words)
+                ]
+                all_identical = all(identical)
+
+                if all_identical:
+                    # Update response & index
+                    if result_llm_item_start:
+                        # Update index
+                        result_llm_item_start = False
+
+                        # Update response
+                        response_item.start_time = start_time
+                    else:
+                        # Update index
+                        result_llm_item_start = True
+                        result_llm_index += 1
+
+                        # Update response
+                        response_item.end_time = end_time
+                        response.root.append(response_item)
+
+                        if result_llm_index >= len(inputData.result_llm):
+                            break
+
+                        response_item = ComputeTimestampsItem(
+                            title=inputData.result_llm[result_llm_index].title,
+                            tags=inputData.result_llm[result_llm_index].tags,
+                            score=inputData.result_llm[result_llm_index].score,
+                            start_time="",
+                            end_time="",
+                        )
+
+                    # Update current item
+                    result_llm_current_words = (
+                        timestamps.get_cleansed_llm_response_item(
+                            list=inputData.result_llm,
+                            index=result_llm_index,
+                            start=result_llm_item_start,
+                        )
+                    )
+
+        if result_llm_index >= len(inputData.result_llm):
+            break
+
+    # Upload result
+    await utils.upload_string(
+        data=response.model_dump_json(),
+        storage_domain_name=settings.STORAGE_DOMAIN_NAME,
+        storage_container_name=settings.STORAGE_CONTAINER_NAME,
+        storage_blob_name=os.path.join(inputData.instance_id, "timestamps.json"),
+    )
     return response
