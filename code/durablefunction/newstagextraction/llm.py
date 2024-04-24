bb50e4cc546ec4d2425bf76948638bc2bd7efa52
Marvin Buss <marvin.buss@gmail.com>
2024-04-24 11:24:13 +0200

    Update prompts

--- code/durablefunction/newstagextraction/llm.py
+++ code/durablefunction/newstagextraction/llm.py
@@ -10,20 +10,21 @@ from models.newstagextraction import InvokeLlmResponse, LlmResponseItem
 
 class LlmMessages:
     SYSTEM_MESSAGE: str = """
-    You are a world class assistant for summarizing news content.
-    You extract sections from the provided news content and generate a title for each section.
-    Each section consists of at least 10 words and all sections are disjoint and non-overlapping.
-    You must include the start and end of the original text for each section in the response. The text snippet describing the beginning and end should include 5 words as well as any punctuation.
-    You add tags to each section. Samples for tags are: sports, weather, international news, national news, politics, crime, technology, celebrity, other. You add additional tags based on the content of each section.
-    For each section you provide a score between 0 and 10 indicating how good the defined tags match the content of the section. 0 indicates that the tags don't match the content, and 10 means that the tags are a perfect match.
-
+    You are a world class assistant for identifying news sections. 
+    You split the provided news content into broad thematic sections. The content of each section must cover a common news topic. 
+    Do the following and provide a valid JSON response that uses the schema mentioned below:
+    1. Split the provided text into broad thematic sections. Each section must start and end with a full sentence. Ensure that all sentences of the provided text are part of exactly one section.
+    2. Identify the start and end of each section and return the first and last sentence of each section.
+    3. Generate a title for each news section.
+    4. Add tags to each subsection. Samples for tags are: sports, weather, international news, national news, politics, crime, technology, celebrity, other. You add additional tags based on the content of each section.
+    5. Generate a score between 0 and 10 for each section. The score indicates how good the defined tags match the content of the section. 0 indicates that the tags don't match the content, and 10 means that the tags are a perfect match.
     Here is a sample JSON response:
     {format_sample}
     """
     USER_MESSAGE: str = """
     News Content: "{news_content}"
     ---
-    Provide a summary for the provided news text. The text is from the following tv show: {news_show_details}
+    Identify news sections for the provided news text according to the instructions. The text is from the following tv show: {news_show_details}
     """
 
 
